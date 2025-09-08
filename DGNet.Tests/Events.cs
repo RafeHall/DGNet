9843d4e6467b0208d860109884488690b74f463c
RafeHall <rafeohall@gmail.com>
2025-09-08 01:42:33 -0700

    Renamed EventGroup.cs to Events.cs

--- DGNet.Tests/Events.cs
+++ DGNet.Tests/Events.cs
@@ -1,4 +1,3 @@
-
 using DGNet.Event;
 
 namespace DGNet.Tests;
@@ -9,35 +8,59 @@ public enum KickReason : byte
     None = 0,
     Cheating = 1,
     Idle = 2,
+    Spamming = 3,
 }
 
 public static class KickReasonImpl
 {
-    public static string KickReasonString(this KickReason reason)
+    public static string ToString(this KickReason reason)
     {
         return reason switch
         {
             KickReason.Cheating => "Cheating",
             KickReason.Idle => "Idle",
+            KickReason.Spamming => "Spamming",
             _ => "No Reason Given",
         };
     }
 }
 
+public enum VoteResult : byte
+{
+    Passed = 0,
+    Failed = 1,
+    NotEnoughVotes = 2,
+}
+
+public static class VoteResultImpl
+{
+    public static string ToString(this VoteResult result)
+    {
+        return result switch
+        {
+            VoteResult.Passed => "Passed",
+            VoteResult.Failed => "Failed",
+            VoteResult.NotEnoughVotes => "Not Enough Votes",
+            _ => "Unknown Result",
+        };
+    }
+}
+
+interface ITest
+{
+    public static abstract void Test();
+}
+
 [GenerateEvents]
 public partial record VoteEvent
 {
-    public partial record BeginMapVote(string[] Maps) : VoteEvent
-    {
-        public bool HasAnyMaps => Maps.Length != 0;
-    }
-
+    public partial record BeginMapVote(string[] Maps) : VoteEvent { }
     public partial record BeginKickVote(string Player, KickReason Reason) : VoteEvent { }
-    public partial record EndVote(string Message, bool Passed) : VoteEvent { }
-
+    public partial record EndVote(string Message, VoteResult Result) : VoteEvent { }
     public partial record ClearVoting(string[] Messages) : VoteEvent { }
 }
 
+// dummy GUI interface
 public interface IVoteMenu
 {
     public string Title { get; set; }
@@ -58,25 +81,19 @@ public class UsageExample
     public UsageExample()
     {
         _menu = null!; // yes
-    }
 
-    public void EnterTree()
-    {
         VoteEvent.BeginMapVote.Event += OnBeginMapEvent;
         VoteEvent.BeginKickVote.Event += OnBeginKickEvent;
         VoteEvent.EndVote.Event += OnVoteEndEvent;
-
         VoteEvent.ClearVoting.Event += OnClearVoteEvent;
     }
 
-    public void ExitTree()
+    ~UsageExample()
     {
         VoteEvent.BeginMapVote.Event -= OnBeginMapEvent;
         VoteEvent.BeginKickVote.Event -= OnBeginKickEvent;
         VoteEvent.EndVote.Event -= OnVoteEndEvent;
-
         VoteEvent.ClearVoting.Event -= OnClearVoteEvent;
-
     }
 
     private void OnClearVoteEvent(VoteEvent.ClearVoting ev)
@@ -87,11 +104,6 @@ public class UsageExample
 
     public void OnBeginMapEvent(VoteEvent.BeginMapVote ev)
     {
-        if (!ev.HasAnyMaps)
-        {
-            return;
-        }
-
         _menu.Reset();
         _menu.Title = "What Map Next?";
 
@@ -106,7 +118,7 @@ public class UsageExample
         _menu.Reset();
         _menu.Title = "Kick Player?";
         _menu.AddText(ev.Player);
-        _menu.AddText(ev.Reason.KickReasonString());
+        _menu.AddText(ev.Reason.ToString());
         _menu.AddChoice("Yes", 1, 0);
         _menu.AddChoice("No", 0, 1);
     }
@@ -115,13 +127,15 @@ public class UsageExample
     {
         _menu.Reset();
         _menu.AddText(ev.Message);
-        if (ev.Passed)
+        _menu.AddText(ev.Result.ToString());
+        switch (ev.Result)
         {
-            _menu.ShowCheck();
-        }
-        else
-        {
-            _menu.ShowCross();
+            case VoteResult.Passed:
+                _menu.ShowCheck();
+                break;
+            case VoteResult.Failed:
+                _menu.ShowCross();
+                break;
         }
         _menu.HideIn(3.0f);
     }
